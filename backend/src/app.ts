f42fc6ad1d6bc910c5d7167a3ac52c09ab7f166f
Jason Martens <jasonmartens2@gmail.com>
2023-06-04 13:29:25 -0600

    Routing and Error Handling

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,31 @@
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import notesRouters from "./routes/notes";
+import morgan from "morgan";
+import createHttpError, { isHttpError } from "http-errors";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello Jason");
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use("/api/notes", notesRouters);
+
+app.use((req, res, next) => {
+  next(createHttpError(404, "Endpoint not found"));
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.log(error);
+  let errorMessage = "An unknown Error occured";
+  let statusCode = 500;
+  if (isHttpError(error)) {
+    statusCode = error.status;
+    errorMessage = error.message;
+  }
+  res.status(statusCode).json({ error: errorMessage });
 });
 
 export default app;
