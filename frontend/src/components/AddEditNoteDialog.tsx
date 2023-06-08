6263d074b7251ee0738720accb9f013265ecb7f1
Jason Martens <jasonmartens2@gmail.com>
2023-06-08 14:03:13 -0600

    Implemented user authentication using express-session

---
+++ frontend/src/components/AddEditNoteDialog.tsx
@@ -0,0 +1,80 @@
+import { Modal, Form, Button } from "react-bootstrap"
+import { useForm } from "react-hook-form";
+import { NoteInput } from "../network/notes_api";
+import * as NotesApi from "../network/notes_api"
+import { Note } from "../models/note";
+
+interface AddEditNoteDialogProps {
+  noteToEdit?: Note,
+  onDismiss: () => void;
+  onNoteSaved: (note: Note) => void;
+}
+
+function AddEditNoteDialog({noteToEdit, onDismiss, onNoteSaved}: AddEditNoteDialogProps) {
+
+  const { register, handleSubmit, formState : { errors, isSubmitting} } = useForm<NoteInput>({
+    defaultValues: {
+      title: noteToEdit?.title || "",
+      text: noteToEdit?.text || "",
+    }
+  });
+
+  async function onSubmit(input: NoteInput) {
+    try {
+      let noteResponse: Note;
+      if (noteToEdit) {
+        noteResponse = await NotesApi.updateNote(noteToEdit._id, input);
+      } else {
+        noteResponse = await NotesApi.createNote(input);
+      }
+
+      onNoteSaved(noteResponse);
+    } catch (error) {
+      console.log(error);
+      alert(error);
+    }
+  }
+
+  return (
+    <Modal show onHide={onDismiss}>
+      <Modal.Header closeButton>
+        <Modal.Title>
+          {noteToEdit ? "Edit Note" : "Add Note"} Note
+        </Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <Form id="addEditNoteForm" onSubmit={handleSubmit(onSubmit)}>
+          <Form.Group className="mb-3">
+            <Form.Label>Title</Form.Label>
+            <Form.Control 
+              type="text"
+              placeholder="Title"
+              isInvalid={!!errors.title}
+              {...register("title", { required: "Required" })}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.title?.message}
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Text</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={5}
+              placeholder="Text"
+              {...register("text")}
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button type="submit" form="addEditNoteForm" disabled={isSubmitting}>
+          Save
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
+
+export default AddEditNoteDialog
--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -1,14 +1,18 @@
 import styles from '../styles/Note.module.css'
+import styleUtils from "../styles/utils.module.css"
 import { Note as NoteModel } from '../models/note'
 import { Card } from 'react-bootstrap'
 import { formatDate } from '../utils/formatDate';
+import { MdDelete } from "react-icons/md"
 
 interface NoteProps {
   note: NoteModel,
+  onNoteClicked: (note: NoteModel) => void,
+  onDeleteNoteCLicked: (note: NoteModel) => void,
   className?: string,
 }
 
-function Note({ note, className }: NoteProps) {
+function Note({ note, onNoteClicked, onDeleteNoteCLicked, className }: NoteProps) {
   const {
     title,
     text,
@@ -25,10 +29,19 @@ function Note({ note, className }: NoteProps) {
   }
 
   return (
-    <Card className={`${styles.noteCard} ${className}`}>
+    <Card 
+    className={`${styles.noteCard} ${className}`}
+    onClick={() => onNoteClicked(note)}>
       <Card.Body className={styles.cardBody}>
-        <Card.Title>
+        <Card.Title className={styleUtils.flexCenter}>
           {title}
+          <MdDelete
+            className="text-muted ms-auto"
+            onClick={(e) => {
+              onDeleteNoteCLicked(note);
+              e.stopPropagation();
+            }}
+          />
         </Card.Title>
         <Card.Text className={styles.noteText}>
           {text}
